36ac4c30bb6b988d35ecc841de7669d10a00f37a
Sergey Revyakin <sarevyakin@gmail.com>
2023-03-27 11:14:20 +0200

    Test update

--- app/src/androidTest/java/com/serg/babbelchallenge/ui/items/TestGameOverScreen.kt
+++ app/src/androidTest/java/com/serg/babbelchallenge/ui/items/TestGameOverScreen.kt
@@ -14,18 +14,31 @@ class TestGameOverScreen : ComposableItemTest() {
         var clickCount = 0
 
         setContent {
-            GameOverScreen(score = 0, correctAnswer = "TestAnswer", isTimeoutLoose = false) {
+            GameOverScreen(score = 10, correctAnswer = "TestAnswer", isTimeoutLoose = false) {
                 clickCount++
             }
         }
 
         val gameOverButton = onNodeWithText("Restart")
-        val answerText = onNodeWithText("Correct Answer Is TestAnswer")
+        val answerText = onNodeWithText("The Right Answer is\nTestAnswer")
+        val scoreText = onNodeWithText("Your Score:10")
 
         gameOverButton.assertIsDisplayed()
         answerText.assertIsDisplayed()
+        scoreText.assertIsDisplayed()
 
         gameOverButton.performClick()
         assertEquals(1, clickCount)
+    }
+
+    @Test
+    fun testGameOverOnTimeout() =runTest {
+        setContent {
+            GameOverScreen(score = 10, correctAnswer = "", isTimeoutLoose = true) {
+            }
+        }
+
+        val gameOverText = onNodeWithText("You have been thinking too long :)")
+        gameOverText.assertIsDisplayed()
     }
 }
